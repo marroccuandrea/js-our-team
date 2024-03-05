02f4b8349b995cb0ca0330190528f13e2c142f7e
marroccuandrea <marroccuandrea@yahoo.it>
2024-03-05 14:54:19 +0100

    [add] array impiegati

---
+++ script.js
@@ -0,0 +1,42 @@
+const output = document.querySelector('.output');
+// Creo un array contenente tutte le informazioni dei dipendenti
+
+const impiegati = [
+    {
+        nome: "Wayne",
+        cognome: "Barnett",
+        ruolo: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        nome: "Angela",
+        cognome: "Caroll",
+        ruolo: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        nome: "Walter",
+        cognome: "Gordon",
+        ruolo: "Office Manager",
+        img: "walter-gordon-office-manager.jpg"
+    },
+    {
+        nome: "Angela",
+        cognome: "Lopez",
+        ruolo: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        nome: "Scott",
+        cognome: "Estrada",
+        ruolo: "Developer",
+        img: "scott-estrada-developer.jpg"
+    },
+    {
+        nome: "Barbara",
+        cognome: "Ramos",
+        ruolo: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg"
+    }
+    
+]
